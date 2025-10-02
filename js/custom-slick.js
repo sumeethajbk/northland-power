a32a1ce7be5789c7ac94f80294503e354fa1102f
sumeetha <sumeetha@jbkinfotech.com>
2025-10-02 18:52:52 +0530

    first commit

--- js/custom-slick.js
+++ js/custom-slick.js
@@ -1,46 +1,91 @@
 jQuery(document).ready(function () {
 
-  /* Sites and Facilities Slider */
-  function SitesSlider() {
-    var $el = jQuery('.sites-facilities-inner'),
-      w = jQuery(window).width(),
-      count = $el.children().length;
+  /* Homa Banner */
+  jQuery('.caption-details, .slide-description').each((_, el) => {
+    const text = jQuery(el).text().replace(/\u00A0/g, ' ').trim();
+    const words = text.split(/\s+/);
+    jQuery(el).html(
+      words.map((w, i) => `<span style="animation-delay:${i * 0.05}s">${w}&nbsp;</span>`).join('')
+    );
+  });
 
-    if ($el.hasClass('slick-initialized')) $el.slick('unslick');
+  const slides = jQuery('.slide').map((_, el) => {
+    const $el = jQuery(el);
+    return {
+      title: $el.find('.caption-details').text(),
+      desc: $el.find('.slide-description').text(),
+      image: $el.find('img').attr('src')
+    };
+  }).get();
 
-    if (w > 767 && count > 3) {
-      $el.removeClass('few-sites').slick({
-        slidesToShow: 1,
-        arrows: true,
-        variableWidth: true,
-        infinite: false,
-        speed: 1000,
-        draggable: true,
-        swipeToSlide: true,
-        touchThreshold: 100,
-        prevArrow: '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><span><i class="fa-light fa-chevron-left"></i></span></div>',
-          nextArrow: '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button"><span><i class="fa-light fa-chevron-right"></i></span></div>',
-      });
-    } else if (w <= 767) {
-      $el.removeClass('few-sites').slick({
-        slidesToShow: 1,
-        arrows: true,
-        variableWidth: true,
-        infinite: false,
-        speed: 1000,
-        draggable: true,
-        swipeToSlide: true,
-        touchThreshold: 100,
-        prevArrow: '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><span><i class="fa-light fa-chevron-left"></i></span></div>',
-          nextArrow: '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button"><span><i class="fa-light fa-chevron-right"></i></span></div>',
-      });
-    } else {
-      $el.addClass('few-sites');
-    }
+  const total = slides.length;
+
+  jQuery('.slide').first().addClass('initial-load');
+  setTimeout(() => jQuery('.slide').first().removeClass('initial-load'), 1000);
+
+  var $sliderm = jQuery('.main-slider');
+  $sliderm.slick({
+    dots: true,
+    infinite: true,
+    speed: 1070,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: 'cubic-bezier(0.645,0.045,0.355,1)',
+    prevArrow: `<div class="slick-arrow slick-prev" aria-label="Previous Arrow" role="button"><div class="arrow-icon"><i class="fa-sharp fa-thin fa-arrow-left"></i></div></div>`,
+    nextArrow: `<div class="slick-arrow slick-next" aria-label="Next Arrow" role="button"><div class="arrow-icon"><i class="fa-sharp fa-thin fa-arrow-right"></i></div></div>`
+  });
+
+  function updatePreviews(currentIndex) {
+    var total = $sliderm.slick('getSlick').slideCount;
+
+    var prevIndex = (currentIndex - 1 + total) % total;
+    var $prevSlide = $sliderm.find('.slide').eq(prevIndex);
+    jQuery('.prev-slick-img img').attr('src', $prevSlide.find('img').attr('src'));
+    jQuery('.prev-slick-caption').html('<p>' + $prevSlide.find('.caption-details').text() + '</p>');
+
+    var nextIndex = (currentIndex + 1) % total;
+    var $nextSlide = $sliderm.find('.slide').eq(nextIndex);
+    jQuery('.next-slick-img img').attr('src', $nextSlide.find('img').attr('src'));
+    jQuery('.next-slick-caption').html('<p>' + $nextSlide.find('.caption-details').text() + '</p>');
   }
 
-  SitesSlider();
-  jQuery(window).on('resize', SitesSlider);
+  setTimeout(function () {
+    jQuery('.slick-prev').prepend(`
+    <div class="prev-slick-img slick-thumb-nav">
+      <div class="thumb-small"><img src="/prev.jpg"></div>
+      <div class="prev-slick-caption"></div>
+    </div>
+  `);
+    jQuery('.slick-next').append(`
+    <div class="next-slick-img slick-thumb-nav">
+      <div class="thumb-small"><img src="/next.jpg"></div>
+      <div class="next-slick-caption"></div>
+    </div>
+  `);
+
+    var current = $sliderm.slick('slickCurrentSlide');
+    updatePreviews(current);
+  }, 500);
+
+  $sliderm
+    .on('beforeChange', function (event, slick, curr, next) {
+      const $c = jQuery('.slide').eq(curr).find('img');
+      $c.css('transform', 'translateY(0) scale(1)');
+      setTimeout(() => $c.css('transform', ''), 875);
+    })
+    .on('afterChange', function (event, slick, currentSlide) {
+      updatePreviews(currentSlide);
+    });
+
+  jQuery('.slider-container .slick-dots li button').each(function () {
+    jQuery(this).html(`
+    <svg width="24" height="24">
+      <circle class="bg" cx="12" cy="12" r="10"></circle>
+      <circle class="progress" cx="12" cy="12" r="10"></circle>
+    </svg>
+  `);
+  });
 
 
   /* Featured News */
